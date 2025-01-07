830552816583fbd9efc34ea5e25a734c3bfdd3dc
PickSquid <m1n0rm1n3r@proton.me>
2025-01-07 14:03:45 -0700

    Revert "Update VSCode config" This change seems to have broken the Continue extension.
    This reverts commit 8b2ffec311b5c31c80341c716c469c17aadfceca.

--- modules/home/development/vscode.nix
+++ modules/home/development/vscode.nix
@@ -6,7 +6,7 @@
   system,
   ...
 }: let
-  pkg = pkgs.unstable.vscodium;
+  pkg = pkgs.vscodium;
 in
   lib.internal.mkOptionalModule config "vscode" {
     home.shellAliases = {
@@ -15,7 +15,6 @@ in
     programs.vscode = {
       enable = true;
       package = pkg;
-      userSettings = lib.importJSON ./vscode.settings.json;
       extensions = with inputs.nix-vscode-extensions.extensions.${system}.vscode-marketplace; [
         # From `codium --list-extensions` on my previous system, with broken/missing extensions commented out
         aaron-bond.better-comments
