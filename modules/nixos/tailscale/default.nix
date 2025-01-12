0ef77ae3caa25e565f8170648d464bd0a733ddaf
PickSquid <m1n0rm1n3r@proton.me>
2025-01-12 01:29:25 -0700

    Disable Tailscale DNS, manually add Tailscale IPs to config Running Tailscale normally causes it to replace the `/etc/resolv.conf` file, which breaks DNS resolution on my system, effectively cutting me off from the Internet. This is a Band-Aid fix until I can figure out how to get Tailscale to provide its DNS features without stepping on NetworkManager's toes.

--- modules/nixos/tailscale/default.nix
+++ modules/nixos/tailscale/default.nix
@@ -10,5 +10,16 @@ in {
   services.tailscale = {
     enable = true;
     authKeyFile = config.sops.secrets.tailscale.path;
+    # HACK: Disable Tailscale DNS for the time being; manual host setup included below
+    extraSetFlags = [
+      "--accept-dns=false"
+    ];
   };
+  # Assign Tailscale IPs to nicknames under a `.tailscale` namespace in the hosts file
+  networking.extraHosts = lib.concatLines (lib.mapAttrsToList (alias: ip: "${ip} ${alias}.tailscale") {
+    aorus = "100.64.230.47";
+    fire = "100.95.13.86";
+    optiplex = "100.70.98.57";
+    phone = "100.77.174.47";
+  });
 })
